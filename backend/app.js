cf213cdc7723afffe2fcbf0273a6a04ea3a8a148
Leonard <leonard.flachs@gmail.com>
2021-02-06 12:42:53 +0100

    Remove frontend Serving

--- backend/app.js
+++ backend/app.js
@@ -45,11 +45,11 @@ const authenticationMiddleware = (req, res, next) => {
 app.get('/secret', authenticationMiddleware, (req, res, next) => {
 	res.status(200).json({ message: 'You found the seret!' });
 });
-app.use(express.static(path.join(__dirname, 'build')));
+// app.use(express.static(path.join(__dirname, 'build')));
 
-app.get('/*', function (req, res) {
-	res.sendFile(path.join(__dirname, 'build', 'index.html'));
-});
+// app.get('/*', function (req, res) {
+// 	res.sendFile(path.join(__dirname, 'build', 'index.html'));
+// });
 app.use('*', (req, res, next) => {
 	const error = new Error('Not found');
 	error.status = 404;
